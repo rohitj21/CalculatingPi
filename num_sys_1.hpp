e5117a345e05a14ce3a63da5c31b5c7abd958710
rohitj21 <rohitj21@iitk.ac.in>
2022-07-31 01:11:56 +0530

    version 1

---
+++ num_sys_1.hpp
@@ -0,0 +1,96 @@
+#define M 32
+
+typedef struct Num
+{
+    char x[2*M];
+
+} num;
+
+void prt(num n){
+    int i =0;
+    while(n.x[i]==0&&i<M-1)i++;
+    for(; i<M; i++) printf("%d", n.x[i]);
+    int t=2*M-1;
+    while(n.x[t]==0 && t>M-1) t--;
+    if(t!=M-1){
+        cout<<'.';
+        for(int i=M; i<=t;i++)
+            printf("%d", n.x[i]);
+    }
+}
+num number(const char k[]){
+    num n;
+    for(int i = 0; i<2*M; i++) n.x[i]=0;
+    int a=0, b=0, c;
+    for(;;b++)if(k[b]=='\0') break;
+    for(;;a++)if(k[a]=='.'||k[a]=='\0') break;
+    if(a==b) c=0;
+    else c = b-a-1;
+    for(int i=0; i<a; i++)n.x[M-a+i] = k[i] - '0';
+    for(int i = 0; i<c; i++)n.x[M+i] = k[i+a+1]-'0';
+    return n;
+}
+num add(num a, num b){
+    num ans;
+    char carry = 0;
+    for(int i= 2*M - 1; i>=0; i--){
+        ans.x[i] = (a.x[i]+b.x[i] + carry)%10;
+        carry  = (a.x[i]+b.x[i] + carry)/10;
+    }
+    return ans;
+}
+num sbt(num a, num b){
+    num ans;
+    char carry = 0;
+    for(int i = 2*M -1; i>=0; i--){
+        ans.x[i] = (a.x[i]-b.x[i]+10 + carry)%10;
+        carry = (a.x[i]-b.x[i]+carry-9)/10;
+    }
+    return ans;
+}
+num neg(num a){
+    return sbt(number("0"), a);
+}
+num lshift(num a, int k);
+num rshift(num a, int k){
+    num ans ;
+    if(k<=0) return lshift(a, -k);
+    for(int i= 2*M - 1-k; i>=0; i--){
+        ans.x[i+k]= a.x[i];
+    }
+    for(int i=k-1;i>=0; i--){
+        ans.x[i] = 0;
+    }
+    return ans;
+}
+num lshift(num a, int k){
+    num ans ;
+    if(!k) return a;
+    if(k<0) return rshift(a, -k);
+    for(int i= k; i<2*M; i++){
+        ans.x[i-k]= a.x[i];
+    }
+    for(int i=0; i<k; i++){
+        ans.x[2*M -k+i] = 0;
+    }
+    return ans;
+}
+num smlt(num a, char b){
+    char carry = 0;
+    num ans ;
+    for( int i= 2*M -1; i>=0; i--){
+        ans.x[i] = (a.x[i] * b +carry )%10;
+        carry = (a.x[i] * b +carry )/10;
+    }
+    return ans;
+
+}
+num mlt(num a, num b){
+    num ans = number("0");
+    for(int i = 2*M -1; i>=0; i--){
+        if(b.x[i]){
+            ans =add(ans, lshift(smlt(a, b.x[i]), M - 1-i));
+        }
+    }
+    return ans;
+}
---
+++ pi_with_char_10.cpp
@@ -0,0 +1,762 @@
+#include<iostream>
+using namespace std;
+#define ll long long int
+class num
+{
+public: 
+    static const int M = 8;
+public:
+    char x[2*M];
+    
+public:
+    //constructors
+num(){}
+num(int n){
+
+    if(n==0){
+        for(int i=0; i<2*M; i++) x[i]=0;
+    }
+    else if(n>0){
+        int i=2*M-1;
+        for(; i>=M; i--) x[i] = 0;
+        for (; i >=0; i--) {x[i]= n %10; n/=10;}
+    }
+    else {
+        n*=-1;
+        int i=2*M-1;
+        for(; i>=M; i--) x[i] = 0;
+        for(; i>=0; i--) { x[i] = 9 - n%10; n/=10;}
+        for(i = M-1; i>=0; i--) {
+            if(x[i]<9) {
+                x[i]++;
+                return ;
+            }
+            else x[i] = 0;
+        }
+    }  
+}
+num(double y){
+    if(y==0){
+        for(int i=0; i<2*M; i++) x[i]=0;
+    }
+    else if(y>0){
+        ll n = y;
+        
+        for (int i=M-1; i >=0; i--) {x[i]= n %10; n/=10;}
+        y = y - ((ll)y);
+        for(int i =M; i<2*M; i++){
+            y = y*10;
+            x[i] = (ll)y;
+            y = y - ((ll)y);
+        }
+    }
+    else {
+        y*=-1.0;
+        int i=M-1;
+        ll n = y;
+        for(; i>=0; i--) { x[i] = 9 - n%10; n/=10;}
+
+        y = y - ((ll)y);
+        for(int i =M; i<2*M; i++){
+            y = y*10;
+            x[i] = 9-(ll)y;
+            y = y - ((ll)y);
+        }
+
+        for(i = 2*M-1; i>=0; i--) {
+            if(x[i]<9) {
+                x[i]++;
+                return ;
+            }
+            else x[i] = 0;
+        }
+    }
+
+    
+}
+num(const char k[]){
+        int a=0, b=0;
+        for(;;b++)if(k[b]=='\0') break;
+        for(;;a++)if(k[a]=='.'||k[a]=='\0') break;
+        b= a==b? 0:b-a-1;
+        if(k[0] == '-'){
+            for(int i = 0; i<2*M; i++) x[i]=9;
+            a--;
+            for(int i=0; i<a; i++) x[M-a+i] -= k[i+1] - '0';
+            for(int i = 0; i<b; i++) x[M+i] -= k[i+a+2]-'0';
+            for(int i=2*M-1; i>=0; i--){
+                if(x[i]<9){x[i]++;return;}
+                x[i] = 0;
+            }
+
+        }
+        else{
+            for(int i = 0; i<2*M; i++) x[i]=0;
+            for(int i=0; i<a; i++)x[M-a+i] = k[i] - '0';
+            for(int i = 0; i<b; i++)x[M+i] = k[i+a+1]-'0';
+        }
+}
+    
+// operators
+        // assignment
+void operator = (num&b){
+    for(int i=0; i<2*M -1; i++) x[i] = b.x[i];return;
+}
+void operator = (int n){
+    
+    if(n==0){
+        for(int i=0; i<2*M; i++) x[i]=0;
+    }
+    else if(n>0){
+        int i=2*M-1;
+        for(; i>=M; i--) x[i] = 0;
+        for (; i >=0; i--) {x[i]= n %10; n/=10;}
+    }
+    else {
+        n*=-1;
+        int i=2*M-1;
+        for(; i>=M; i--) x[i] = 0;
+        for(; i>=0; i--) { x[i] = 9 - n%10; n/=10;}
+        for(i = M-1; i>=0; i--) {
+            if(x[i]<9) {
+                x[i]++;
+                return ;
+            }
+            else x[i] = 0;
+        }
+    }
+}
+void operator = (double y){
+       
+    if(y==0){
+        for(int i=0; i<2*M; i++) x[i]=0;
+    }
+    else if(y>0){
+        int n = y;
+        
+        for (int i=M-1; i >=0; i--) {x[i]= n %10; n/=10;}
+        y = y - ((int)y);
+        for(int i =M; i<2*M; i++){
+            y = y*10;
+            x[i] = (int)y;
+            y = y - ((int)y);
+        }
+    }
+    else {
+        y*=-1.0;
+        int i=M-1;
+        int n = y;
+        for(; i>=0; i--) { x[i] = 9 - n%10; n/=10;}
+
+        y = y - ((int)y);
+        for(int i =M; i<2*M; i++){
+            y = y*10;
+            x[i] = 9-(int)y;
+            y = y - ((int)y);
+        }
+
+        for(i = 2*M-1; i>=0; i--) {
+            if(x[i]<9) {
+                x[i]++;
+                return ;
+            }
+            else x[i] = 0;
+        }
+    }
+
+}
+void operator =(const char k[]){
+            int a=0, b=0;
+        for(;;b++)if(k[b]=='\0') break;
+        for(;;a++)if(k[a]=='.'||k[a]=='\0') break;
+        b= a==b? 0:b-a-1;
+        if(k[0] == '-'){
+            for(int i = 0; i<2*M; i++) x[i]=9;
+            a--;
+            for(int i=0; i<a; i++) x[M-a+i] -= k[i+1] - '0';
+            for(int i = 0; i<b; i++) x[M+i] -= k[i+a+2]-'0';
+            for(int i=2*M-1; i>=0; i--){
+                if(x[i]<9){x[i]++;return;}
+                x[i] = 0;
+            }
+
+        }
+        else{
+            for(int i = 0; i<2*M; i++) x[i]=0;
+            for(int i=0; i<a; i++)x[M-a+i] = k[i] - '0';
+            for(int i = 0; i<b; i++)x[M+i] = k[i+a+1]-'0';
+        }
+}
+
+    // increment 
+void operator ++ (int){
+    for(int i=M-1; i>=0; i--) {
+        if(x[i]!= 9) {
+            x[i]++;
+            return;
+        }
+        x[i]=0;
+    }
+}
+void operator += (num& b){
+    char carry = 0, t;
+    for(int i= 2*M -1; i>=0; i--){
+        t = (x[i] + carry + b.x[i]);
+        x[i] = t%10;
+        carry = t/10;
+    }
+}
+void operator += (int n){
+    num b(n);
+    char carry = 0, t;
+    for(int i= 2*M -1; i>=0; i--){
+        t = (x[i] + carry + b.x[i]);
+        x[i] = t%10;
+        carry = t/10;
+    }
+}
+void operator +=(double y){
+    num b(y);
+    char carry = 0, t;
+    for(int i= 2*M -1; i>=0; i--){
+        t = (x[i] + carry + b.x[i]);
+        x[i] = t%10;
+        carry = t/10;
+    }
+}
+    // decrement 
+void operator -- (int){
+
+    for(int i=M-1; i>=0; i--) {
+        if(x[i]) {
+            x[i]--;
+            return;
+        }
+        x[i]=9;
+    }
+
+}
+void operator -= (num& b){
+    char carry = 0, t;
+    for(int i = 2*M -1; i>=0; i--){
+        t = (x[i]- b.x[i]+ carry+10);
+        x[i] = t%10;
+        carry = (t-19)/10;
+    }
+}
+void operator -= (int n){
+    num b(n);
+    char carry = 0, t;
+    for(int i = 2*M -1; i>=0; i--){
+        t = (x[i]- b.x[i]+ carry+10);
+        x[i] = t%10;
+        carry = (t-19)/10;
+    }
+}
+void operator -=(double y){
+    num b(y);
+    char carry = 0, t;
+    for(int i = 2*M -1; i>=0; i--){
+        t = (x[i]- b.x[i]+ carry+10);
+        x[i] = t%10;
+        carry = (t-19)/10;
+    }
+}
+
+    // addition
+num operator + (num& b){
+    num ans;
+    char carry = 0;
+    for(int i= 2*M - 1; i>=0; i--){
+        ans.x[i] = (x[i]+b.x[i] + carry)%10;
+        carry  = (x[i]+b.x[i] + carry)/10;
+    }
+    return ans;
+}
+    // negetion
+num operator - (){
+    num b;
+    int j=2*M-1;
+    for(;!x[j]; j--)
+        b.x[j] =0;
+    if(j>=0)b.x[j] = 10 -x[j];
+    for(j--;j>=0; j--) 
+        b.x[j] =9 - x[j];
+    return b;
+}
+    // subtraction
+num operator - (num& b){
+    num ans;
+    char carry = 0;
+    for(int i = 2*M -1; i>=0; i--){
+        ans.x[i] = (x[i]-b.x[i]+10 + carry)%10;
+        carry = (x[i]-b.x[i]+carry-9)/10;
+    }
+    return ans;
+}
+    // comparision
+bool operator == (num& b){
+    for(int i =2*M-1; i>=0; i--){
+        if(x[i]!=b.x[i]) return false;
+    }
+    return true;
+}
+bool operator == (int n){
+    num b(n);
+        for(int i =2*M-1; i>=0; i--){
+        if(x[i]!=b.x[i]) return false;
+    }
+    return true;
+}
+bool operator == (double y){
+    num b(y);
+    for(int i =2*M-1; i>=0; i--){
+        if(x[i]!=b.x[i]) return false;
+    }
+    return true;
+    
+}
+bool operator == (const char k[]){
+    num b(k);
+    for(int i =2*M-1; i>=0; i--){
+        if(x[i]!=b.x[i]) return false;
+    }
+    return true;
+    
+}
+
+bool operator != (num& b){
+    for(int i =2*M-1; i>=0; i--){
+        if(x[i]!=b.x[i]) return true;
+    }
+    return false;
+}
+bool operator != (int n){
+    num b(n);
+        for(int i =2*M-1; i>=0; i--){
+        if(x[i]!=b.x[i]) return true;
+    }
+    return false;
+}
+bool operator != (double y){
+    num b(y);
+    for(int i =2*M-1; i>=0; i--){
+        if(x[i]!=b.x[i]) return true;
+    }
+    return false;
+    
+}
+bool operator != (const char k[]){
+    num b(k);
+    for(int i =2*M-1; i>=0; i--){
+        if(x[i]!=b.x[i]) return true;
+    }
+    return false;
+    
+}
+
+bool operator > (num& b){
+    if(x[0]>=5 && b.x[0] < 5) return  false;
+    if(x[0]<=5 && b.x[0] > 5) return  true;
+    if(x[0]<=5 && b.x[0] < 5) {
+        for(int i=0; i<2*M; i++){
+            if(x[i] != b.x[i]){
+                return x[i]>b.x[i];
+            }
+        }
+        return false;
+    }
+    
+    for(int i=0; i<2*M; i++){
+        if(x[i] != b.x[i]){
+            return x[i]<b.x[i];
+        }
+    }
+        return false;
+}
+bool operator > (int n){
+    if(!n){
+        if(x[0]) return  x[0]<5;
+        else for(int i=1; i<2*M;i++) if(x[i]) return  true;
+        return false;
+    }
+    if(x[0]>=5 && n>0) return  false;
+    if(x[0]<=5 && n<0) return  true;
+    if(x[0]<=5 && n>0) {
+        num b(n);
+        for(int i=0; i<2*M; i++){
+            if(x[i] != b.x[i]){
+                return x[i]>b.x[i];
+            }
+        }
+        return false;
+    }
+    num b(n);
+    for(int i=0; i<2*M; i++){
+        if(x[i] != b.x[i]){
+            return x[i]<b.x[i];
+        }
+    }
+        return false;
+}
+bool operator > (double y){
+    if(!y){
+        if(x[0]) return  x[0]<5;
+        else for(int i=1; i<2*M;i++) if(x[i]) return  true;
+        return false;
+    }
+    if(x[0]>=5 && y>0) return  false;
+    if(x[0]<=5 && y<0) return  true;
+    if(x[0]<=5 && y>0) {
+        num b(y);
+        for(int i=0; i<2*M; i++){
+            if(x[i] != b.x[i]){
+                return x[i]>b.x[i];
+            }
+        }
+        return false;
+    }
+    num b(y);
+    for(int i=0; i<2*M; i++){
+        if(x[i] != b.x[i]){
+            return x[i]<b.x[i];
+        }
+    }
+        return false;
+}
+bool operator > (const char k[]){
+    num b(k);
+    if(x[0]>=5 && b.x[0] < 5) return  false;
+    if(x[0]<=5 && b.x[0] > 5) return  true;
+    if(x[0]<=5 && b.x[0] < 5) {
+        for(int i=0; i<2*M; i++){
+            if(x[i] != b.x[i]){
+                return x[i]>b.x[i];
+            }
+        }
+        return false;
+    }
+    
+    for(int i=0; i<2*M; i++){
+        if(x[i] != b.x[i]){
+            return x[i]<b.x[i];
+        }
+    }
+        return false;
+}
+
+bool operator >= (num& b){
+    if(x[0]>=5 && b.x[0] < 5) return  false;
+    if(x[0]<=5 && b.x[0] > 5) return  true;
+    if(x[0]<=5 && b.x[0] < 5) {
+        for(int i=0; i<2*M; i++){
+            if(x[i] != b.x[i]){
+                return x[i]>b.x[i];
+            }
+        }
+        return true;
+    }
+    
+    for(int i=0; i<2*M; i++){
+        if(x[i] != b.x[i]){
+            return x[i]<b.x[i];
+        }
+    }
+        return true;
+}
+bool operator >= (int n){
+    if(!n) return x[0]<5;
+    if(x[0]>=5 && n>0) return  false;
+    if(x[0]<=5 && n<0) return  true;
+    if(x[0]<=5 && n>0) {
+        num b(n);
+        for(int i=0; i<2*M; i++){
+            if(x[i] != b.x[i]){
+                return x[i]>b.x[i];
+            }
+        }
+        return true;
+    }
+    num b(n);
+    for(int i=0; i<2*M; i++){
+        if(x[i] != b.x[i]){
+            return x[i]<b.x[i];
+        }
+    }
+        return true;
+}
+bool operator >= (double y){
+    if(!y) return x[0] < 5;
+    if(x[0]>=5 && y>0) return  false;
+    if(x[0]<=5 && y<0) return  true;
+    if(x[0]<=5 && y>0) {
+        num b(y);
+        for(int i=0; i<2*M; i++){
+            if(x[i] != b.x[i]){
+                return x[i]>b.x[i];
+            }
+        }
+        return true;
+    }
+    num b(y);
+    for(int i=0; i<2*M; i++){
+        if(x[i] != b.x[i]){
+            return x[i]<b.x[i];
+        }
+    }
+        return true;
+}
+bool operator >= (const char k[]){
+    num b(k);
+    if(x[0]>=5 && b.x[0] < 5) return  false;
+    if(x[0]<=5 && b.x[0] > 5) return  true;
+    if(x[0]<=5 && b.x[0] < 5) {
+        for(int i=0; i<2*M; i++){
+            if(x[i] != b.x[i]){
+                return x[i]>b.x[i];
+            }
+        }
+        return true;
+    }
+    
+    for(int i=0; i<2*M; i++){
+        if(x[i] != b.x[i]){
+            return x[i]<b.x[i];
+        }
+    }
+        return true;
+}
+
+
+bool operator <= (num& b){
+    if(x[0]>=5 && b.x[0] < 5) return  true;
+    if(x[0]<=5 && b.x[0] > 5) return  false;
+    if(x[0]<=5 && b.x[0] < 5) {
+        for(int i=0; i<2*M; i++){
+            if(x[i] != b.x[i]){
+                return x[i]<b.x[i];
+            }
+        }
+        return true;
+    }
+    
+    for(int i=0; i<2*M; i++){
+        if(x[i] != b.x[i]){
+            return x[i]>b.x[i];
+        }
+    }
+        return true;
+}
+bool operator <= (int n){
+    if(!n){
+        if(x[0]) return  x[0]>=5;
+        else for(int i=1; i<2*M;i++) if(x[i]) return  false;
+        return true;
+    }
+    if(x[0]>=5 && n>0) return  true;
+    if(x[0]<=5 && n<0) return  false;
+    if(x[0]<=5 && n>0) {
+        num b(n);
+        for(int i=0; i<2*M; i++){
+            if(x[i] != b.x[i]){
+                return x[i]<b.x[i];
+            }
+        }
+        return true;
+    }
+    num b(n);
+    for(int i=0; i<2*M; i++){
+        if(x[i] != b.x[i]){
+            return x[i]>b.x[i];
+        }
+    }
+        return true;
+}
+bool operator <= (double y){
+    if(!y){
+        if(x[0]) return  x[0]>=5;
+        else for(int i=1; i<2*M;i++) if(x[i]) return  false;
+        return true;
+    }
+    if(x[0]>=5 && y>0) return  true;
+    if(x[0]<=5 && y<0) return  false;
+    if(x[0]<=5 && y>0) {
+        num b(y);
+        for(int i=0; i<2*M; i++){
+            if(x[i] != b.x[i]){
+                return x[i]<b.x[i];
+            }
+        }
+        return true;
+    }
+    num b(y);
+    for(int i=0; i<2*M; i++){
+        if(x[i] != b.x[i]){
+            return x[i]>b.x[i];
+        }
+    }
+        return true;
+}
+bool operator <= (const char k[]){
+    num b(k);
+    if(x[0]>=5 && b.x[0] < 5) return  true;
+    if(x[0]<=5 && b.x[0] > 5) return  false;
+    if(x[0]<=5 && b.x[0] < 5) {
+        for(int i=0; i<2*M; i++){
+            if(x[i] != b.x[i]){
+                return x[i]<b.x[i];
+            }
+        }
+        return true;
+    }
+    
+    for(int i=0; i<2*M; i++){
+        if(x[i] != b.x[i]){
+            return x[i]>b.x[i];
+        }
+    }
+        return true;
+}
+
+bool operator < (num& b){
+    if(x[0]>=5 && b.x[0] < 5) return  false;
+    if(x[0]<=5 && b.x[0] > 5) return  true;
+    if(x[0]<=5 && b.x[0] < 5) {
+        for(int i=0; i<2*M; i++){
+            if(x[i] != b.x[i]){
+                return x[i]>b.x[i];
+            }
+        }
+        return true;
+    }
+    
+    for(int i=0; i<2*M; i++){
+        if(x[i] != b.x[i]){
+            return x[i]<b.x[i];
+        }
+    }
+        return true;
+}
+bool operator < (int n){
+    if(!n) return x[0]<5;
+    if(x[0]>=5 && n>0) return  false;
+    if(x[0]<=5 && n<0) return  true;
+    if(x[0]<=5 && n>0) {
+        num b(n);
+        for(int i=0; i<2*M; i++){
+            if(x[i] != b.x[i]){
+                return x[i]>b.x[i];
+            }
+        }
+        return true;
+    }
+    num b(n);
+    for(int i=0; i<2*M; i++){
+        if(x[i] != b.x[i]){
+            return x[i]<b.x[i];
+        }
+    }
+        return true;
+}
+bool operator < (double y){
+    if(!y) return x[0] < 5;
+    if(x[0]>=5 && y>0) return  false;
+    if(x[0]<=5 && y<0) return  true;
+    if(x[0]<=5 && y>0) {
+        num b(y);
+        for(int i=0; i<2*M; i++){
+            if(x[i] != b.x[i]){
+                return x[i]>b.x[i];
+            }
+        }
+        return true;
+    }
+    num b(y);
+    for(int i=0; i<2*M; i++){
+        if(x[i] != b.x[i]){
+            return x[i]<b.x[i];
+        }
+    }
+        return true;
+}
+bool operator < (const char k[]){
+    num b(k);
+    if(x[0]>=5 && b.x[0] < 5) return  false;
+    if(x[0]<=5 && b.x[0] > 5) return  true;
+    if(x[0]<=5 && b.x[0] < 5) {
+        for(int i=0; i<2*M; i++){
+            if(x[i] != b.x[i]){
+                return x[i]>b.x[i];
+            }
+        }
+        return true;
+    }
+    
+    for(int i=0; i<2*M; i++){
+        if(x[i] != b.x[i]){
+            return x[i]<b.x[i];
+        }
+    }
+        return true;
+}
+
+
+    // usefull functions
+void prt(){
+        if(x[0]<5){
+            int i =0;
+            while(x[i]==0&&i<M-1)i++;
+            for(; i<M; i++) printf("%d", x[i]);
+            int t=2*M-1;
+            while(x[t]==0 && t>M-1) t--;
+            if(t!=M-1){
+                std:: cout<<'.';
+                for(int i=M; i<=t;i++)  printf("%d", x[i]);
+            }
+        }
+
+        else{
+            num temp(0);
+            std:: cout<<'-';
+            int i=2*M-1;
+            while(!x[i])i--;
+            temp.x[i] = 10-x[i];
+            for(i--; i>=0; i--) temp.x[i] = 9 - x[i];
+            i =0;
+            while(temp.x[i]==0 && i<M-1) i++;
+            for(; i<M; i++) printf("%d", temp.x[i]);
+            int t=2*M-1;
+            while(temp.x[t]==0 && t>M-1) t--;
+            if(t!=M-1){
+                std:: cout<<'.';
+                for(int i=M; i<=t;i++)  printf("%d", temp.x[i]);
+            }
+
+
+            
+        }
+    }
+static num MIN_NUM(){
+
+        num ans;
+        int i;
+        for(i =0; i<2*M; i++){
+            ans.x[i] = 0;
+        }
+        ans.x[i-1] = 1;
+        return ans;
+}
+};
+
+void prt(num& a){
+    a.prt();
+}
+
+int main(){
+    num a= "4.11";
+    num c = -1000;
+    if(a==6);
+    c -= a;
+    prt(c);
+    return 0;
+}
